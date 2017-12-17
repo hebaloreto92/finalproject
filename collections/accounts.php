d155b20f0392ae11e90075ffb7359df1255b37fc
hebaloreto92 <31828352+hebaloreto92@users.noreply.github.com>
2017-12-16 23:12:54 -0500

    Update accounts.php

--- collections/accounts.php
+++ collections/accounts.php
@@ -4,7 +4,7 @@ class accounts extends \database\collection
 {
     protected static $modelName = 'account';
 
-    //This is the function to write to find user by ID for login.
+    //This is the function to write to find the user by ID for login.
     //Don't forget to return the object see findOne in the collection class
 
 
